bf21e7a53d68adab93631c3e1d22a1b563c6b064
Myldhs <1587809027@qq.com>
2020-02-27 18:01:27 +0800

    Initial commit

---
+++ dynweb/src/com/atguigu/ideagittest/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.ideagittest;
+
+/**
+ * @author ldh
+ * @create 2020-02-27 17:59
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+
+}
