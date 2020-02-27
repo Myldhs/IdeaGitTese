86e27684db0624762a90e8aa885e78a15cbbbaeb
Myldhs <51489027+Myldhs@users.noreply.github.com>
2020-02-27 18:41:15 +0800

    pull测试

--- dynweb/src/com/atguigu/ideagittest/GitTest.java
+++ dynweb/src/com/atguigu/ideagittest/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("push测试");
+        System.out.println("pull测试");
 
 
     }
